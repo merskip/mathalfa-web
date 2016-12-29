98dd542a9a0ca587a16dc168a0e654648c373364
Piotr Merski <merskip@gmail.com>
2016-12-29 20:32:33 +0100

    Dodanie mathalfa-base z przykładem użycia

--- src/main/java/pl/merskip/mathalfa/web/HelloServlet.java
+++ src/main/java/pl/merskip/mathalfa/web/HelloServlet.java
@@ -1,5 +1,10 @@
 package pl.merskip.mathalfa.web;
 
+import pl.merskip.mathalfa.base.core.Symbol;
+import pl.merskip.mathalfa.base.infixparser.PostfixParser;
+import pl.merskip.mathalfa.base.operation.CalculateOperation;
+import pl.merskip.mathalfa.base.shared.SharedPostfixParser;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,15 +16,31 @@ import java.io.PrintWriter;
 @WebServlet("")
 public class HelloServlet extends HttpServlet {
     
+    private PostfixParser parser;
+    
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        parser = new SharedPostfixParser();
+    }
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        String plainText = "1+2";
+        
         PrintWriter writer = resp.getWriter();
         writer.println("<!DOCTYPE html>");
         writer.println("<html>");
         writer.println("<body>");
         writer.println("<p>Hello World!</p>");
+        writer.println("<p>" + plainText + " = " + calculate(plainText) + "</p>");
         writer.println("</body>");
         writer.println("</html>");
+    }
+    
+    private String calculate(String plainText) {
+        Symbol rootSymbol = parser.parseAndGetRootSymbol(plainText);
+        return new CalculateOperation().executeForResult(rootSymbol).toPlainText();
     }
 }
